7cd95bd4fad727e42177a2239942e619eb85316b
William R. Arellano <arellanowr@gmail.com>
2025-09-19 14:59:33 -0500

    Remove print from example

--- example/app/Main.hs
+++ example/app/Main.hs
@@ -4,14 +4,12 @@ module Main (main) where
 
 import Data.Aeson
 import Data.Text
-import Text.Megaparsec
 import Text.Mustache
 import qualified Data.Text.Lazy.IO as TIO
 
 main :: IO ()
 main = do
   template <- compileMustacheDir "main" "templates"
-  print template
   TIO.putStr $ renderMustache template $ object
       [ "subnets" .= ["pen" :: Text, "candle", "egg"]
       ]
